ab4e87f82830b6d27f38741abe3917bd7b23abeb
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-11-04 19:27:07 +0530

    updated version

--- src/index.ts
+++ src/index.ts
@@ -3,50 +3,144 @@ export default function transform(file, api, options) {
   const root = j(file.source);
   let dirtyFlag = false;
 
-  // Handle CommonJS require statements
-  root.find(j.VariableDeclarator, {
-    init: {
-      callee: { name: 'require' },
-      arguments: [{ value: 'copy-webpack-plugin' }],
-    },
-  }).forEach((path) => {
-    const localName = path.node.id.name;
-    path.node.init.arguments[0].value = '@rspack/core';
-    path.node.id.name = 'rspack';
-    dirtyFlag = true;
+  // Set of supported plugins, including partially supported ones
+  const supportedPlugins = new Set([
+      'BannerPlugin',
+      'DefinePlugin',
+      'EnvironmentPlugin',
+      'HotModuleReplacementPlugin',
+      'IgnorePlugin',
+      'LimitChunkCountPlugin',
+      'ModuleFederationPlugin',
+      'NormalModuleReplacementPlugin',
+      'ProvidePlugin',
+      'SourceMapDevToolPlugin',
+      'NodeTemplatePlugin',
+      'NodeTargetPlugin',
+      'ElectronTargetPlugin',
+      'EnableChunkLoadingPlugin',
+      'EnableLibraryPlugin',
+      'EnableWasmLoadingPlugin',
+      'ExternalsPlugin',
+      'FetchCompileAsyncWasmPlugin',
+      'EvalSourceMapDevToolPlugin',
+      'ModuleConcatenationPlugin',
+      'NoEmitOnErrorsPlugin',
+      'NodeEnvironmentPlugin',
+      'WebWorkerTemplatePlugin',
+      'EvalDevToolModulePlugin',
+      'APIPlugin',
+      'ConstPlugin',
+      'CommonJsPlugin',
+      'MergeDuplicateChunksPlugin',
+      'RemoveEmptyChunksPlugin',
+      'ConsumeSharedPlugin',
+      'ContainerPlugin',
+      'ContainerReferencePlugin',
+      'EntryOptionPlugin',
+      'JavascriptModulesPlugin',
+      'LoaderOptionsPlugin',
+      'NaturalChunkIdsPlugin',
+      'NaturalModuleIdsPlugin',
+      'RuntimeChunkPlugin',
+      'SideEffectsFlagPlugin',
+      // Partially supported plugins
+      'ProgressPlugin',
+      'SplitChunksPlugin',
+      'EntryPlugin',
+      'RealContentHashPlugin',
+      'DeterministicChunkIdsPlugin',
+      'DeterministicModuleIdsPlugin',
+      'DynamicEntryPlugin',
+      'NamedChunkIdsPlugin',
+      'NamedModuleIdsPlugin',
+      'ProvideSharedPlugin',
+  ]);
 
-    // Replace new CopyWebpackPlugin with new rspack.CopyRspackPlugin
-    root.find(j.NewExpression, {
-      callee: { name: localName },
-    }).forEach((newPath) => {
-      newPath.node.callee = j.memberExpression(
-        j.identifier('rspack'),
-        j.identifier('CopyRspackPlugin'),
-      );
+  // Replace `webpack` with `rspack` in require statements
+  root.find(j.VariableDeclarator, {
+      id: { name: 'webpack' },
+      init: {
+          callee: { name: 'require' },
+          arguments: [{ value: 'webpack' }],
+      },
+  }).forEach((path) => {
+      path.node.id.name = 'rspack';
+      path.node.init.arguments[0].value = '@rspack/core';
       dirtyFlag = true;
-    });
   });
 
-  // Handle ES6 import statements
+  // Replace `import webpack from 'webpack';` with `import rspack from '@rspack/core';`
   root.find(j.ImportDeclaration, {
-    source: { value: 'copy-webpack-plugin' },
+      source: { value: 'webpack' },
   }).forEach((path) => {
-    const localName = path.node.specifiers[0].local.name;
-    path.node.source.value = '@rspack/core';
-    path.node.specifiers[0].local.name = 'rspack';
-    path.node.specifiers[0].imported = null;
-    dirtyFlag = true;
-
-    // Replace new CopyPlugin with new rspack.CopyRspackPlugin
-    root.find(j.NewExpression, {
-      callee: { name: localName },
-    }).forEach((newPath) => {
-      newPath.node.callee = j.memberExpression(
-        j.identifier('rspack'),
-        j.identifier('CopyRspackPlugin'),
-      );
+      path.node.source.value = '@rspack/core';
+      path.node.specifiers.forEach((specifier) => {
+          if (specifier.local.name === 'webpack') {
+              specifier.local.name = 'rspack';
+          }
+      });
       dirtyFlag = true;
-    });
+  });
+
+  // Handle destructured require statements like `const { ProvidePlugin } = require('webpack');`
+  root.find(j.VariableDeclarator, {
+      init: {
+          callee: { name: 'require' },
+          arguments: [{ value: 'webpack' }],
+      },
+  }).forEach((path) => {
+      if (j.ObjectPattern.check(path.node.id)) {
+          path.node.init.arguments[0].value = '@rspack/core';
+          path.node.id.properties.forEach((property) => {
+              if (supportedPlugins.has(property.key.name)) {
+                  dirtyFlag = true;
+              }
+          });
+      }
+  });
+
+  // Replace `new ProvidePlugin({})` with `new rspack.ProvidePlugin({})`
+  root.find(j.NewExpression, {
+      callee: {
+          name: (name) => supportedPlugins.has(name),
+      },
+  }).forEach((path) => {
+      if (supportedPlugins.has(path.node.callee.name)) {
+          path.node.callee = j.memberExpression(
+              j.identifier('rspack'),
+              j.identifier(path.node.callee.name),
+          );
+          dirtyFlag = true;
+      }
+  });
+
+  // Replace `new webpack.*Plugin` with `new rspack.*Plugin` for supported plugins
+  root.find(j.NewExpression, {
+      callee: {
+          object: { name: 'webpack' },
+          property: { name: (name) => supportedPlugins.has(name) },
+      },
+  }).forEach((path) => {
+      if (j.MemberExpression.check(path.node.callee)) {
+          path.node.callee.object.name = 'rspack';
+          dirtyFlag = true;
+      }
+  });
+
+  // General check for nested plugins like `webpack.anything.*Plugin`
+  root.find(j.NewExpression, {
+      callee: {
+          object: {
+              object: { name: 'webpack' },
+          },
+          property: { name: (name) => supportedPlugins.has(name) },
+      },
+  }).forEach((path) => {
+      if (j.MemberExpression.check(path.node.callee)) {
+          path.node.callee.object.object.name = 'rspack';
+          dirtyFlag = true;
+      }
   });
 
   return dirtyFlag ? root.toSource() : undefined;
